10db6651fe66d515ffec7fd199c19b7e4f5fe12b
Yonatan Krieger <yonatan.k@walkme.com>
2017-10-31 12:09:55 +0200

    changing port

--- src/components/events/serverEventsHandler.js
+++ src/components/events/serverEventsHandler.js
@@ -1,5 +1,5 @@
 import openSocket from 'socket.io-client';
-const socket = openSocket(`${process.env.REACT_APP_API_URL}:8000`);
+const socket = openSocket(`${process.env.REACT_APP_API_URL}:8001`);
 
 const clientCallbacks = [];
 
