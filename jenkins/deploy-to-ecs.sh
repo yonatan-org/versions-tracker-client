06e75ceaf239ebcb6ba03bc544c48a98e45b41f9
Yonatan Krieger <yonatan.k@walkme.com>
2018-11-27 17:07:50 +0200

    More changes

--- jenkins/deploy-to-ecs.sh
+++ jenkins/deploy-to-ecs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-IMAGE_DATE_TAG=$1
 
-python /usr/local/bin/ecs deploy prod-automation-cluster tracker-client --tag bluehotdog/versions_tracker_client:$IMAGE_DATE_TAG --access-key-id ${AWS_ACCESS_KEY_ID}  --secret-access-key ${AWS_SECRET_ACCESS_KEY} --region us-west-2 --timeout 900 --rollback
+
+python /usr/local/bin/ecs deploy prod-automation-cluster tracker-client --access-key-id ${AWS_ACCESS_KEY_ID}  --secret-access-key ${AWS_SECRET_ACCESS_KEY} --region us-west-2 --timeout 900 --rollback
--- jenkins/push-image.sh
+++ jenkins/push-image.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-IMAGE_DATE_TAG=$1
 
-docker build -t bluehotdog/versions_tracker_client:$IMAGE_DATE_TAG .
+docker build -t bluehotdog/versions_tracker_client .
 
-docker push  bluehotdog/versions_tracker_client:$IMAGE_DATE_TAG
+docker push  bluehotdog/versions_tracker_client
