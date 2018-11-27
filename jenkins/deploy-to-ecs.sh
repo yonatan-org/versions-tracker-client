69a9471d631207b3c2227a2dd81686860350d48e
Yonatan Krieger <yonatan.k@walkme.com>
2018-11-27 16:26:30 +0200

    More changes

--- jenkins/deploy-to-ecs.sh
+++ jenkins/deploy-to-ecs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python /usr/local/bin/ecs deploy prod-automation-cluster tracker-client --tag bluehotdog/versions_tracker_client:latest --access-key-id ${AWS_ACCESS_KEY_ID}  --secret-access-key ${AWS_SECRET_ACCESS_KEY} --region $REGION
+python /usr/local/bin/ecs deploy prod-automation-cluster tracker-client --tag bluehotdog/versions_tracker_client:latest --access-key-id ${AWS_ACCESS_KEY_ID}  --secret-access-key ${AWS_SECRET_ACCESS_KEY} --region us-west-2 --timeout 900 --rollback
