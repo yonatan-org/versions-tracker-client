1f4f243abb65da1d4f99c1df9853b5255d17a4a0
Yonatan Krieger <yonatan.k@walkme.com>
2018-11-28 12:26:51 +0200

    force pushing

--- jenkins/deploy-to-ecs.sh
+++ jenkins/deploy-to-ecs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python /usr/local/bin/ecs deploy prod-automation-cluster tracker-client --access-key-id ${AWS_ACCESS_KEY_ID}  --secret-access-key ${AWS_SECRET_ACCESS_KEY} --region us-west-2 --timeout 900 --rollback --force-new-deployment
+python /usr/local/bin/ecs update-service --cluster prod-automation-cluster --service tracker-client --access-key-id ${AWS_ACCESS_KEY_ID} --secret-access-key ${AWS_SECRET_ACCESS_KEY} --region us-west-2 --timeout 900 --rollback --force-new-deployment
