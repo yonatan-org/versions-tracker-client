885da7dd99186e3248260e75205bba6d6b95f3ab
Yonatan Krieger <yonatan.k@walkme.com>
2018-11-28 11:14:55 +0200

    trying to fix the images

--- jenkins/deploy-to-ecs.sh
+++ jenkins/deploy-to-ecs.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-BUILD_NUMBER=$1
-
-python /usr/local/bin/ecs deploy prod-automation-cluster tracker-client -t bluehotdog/versions_tracker_client:$BUILD_NUMBER --access-key-id ${AWS_ACCESS_KEY_ID}  --secret-access-key ${AWS_SECRET_ACCESS_KEY} --region us-west-2 --timeout 900 --rollback
+python /usr/local/bin/ecs deploy prod-automation-cluster tracker-client --access-key-id ${AWS_ACCESS_KEY_ID}  --secret-access-key ${AWS_SECRET_ACCESS_KEY} --region us-west-2 --timeout 900 --rollback
--- jenkins/push-image.sh
+++ jenkins/push-image.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-BUILD_NUMBER=$1
+docker build -t bluehotdog/versions_tracker_client .
 
-docker build -t bluehotdog/versions_tracker_client:$BUILD_NUMBER .
-
-docker push bluehotdog/versions_tracker_client:$BUILD_NUMBER
+docker push  bluehotdog/versions_tracker_client
